585842355ef37c95477646507242813f1a74b3a5
Lunaticsky-tql <2013599@mail.nankai.edu.cn>
2022-05-08 00:04:45 +0800

    add deepl translation

--- init.lua
+++ init.lua
@@ -44,7 +44,42 @@ hyper:bind({}, "w", function()
 end)
 
 hyper:bind({}, "d", function()
-  x = hs.application.open("/Applications/Deepl.app")
+  --get the text in the pasteboard
+  local text = hs.pasteboard.getContents() 
+  local translate_script=[[
+    global translation_result
+    tell application "System Events"
+      tell process "DeepL"
+        local previous_result     
+        set previous_result to value of text area 1 of group 5 of group 4 of group 1 of UI element 1 of scroll area 1 of group 1 of group 1 of window "DeepL"
+        set value of text area 1 of group 4 of group 2 of group 1 of UI element 1 of scroll area 1 of group 1 of group 1 of window "DeepL" to "]]..text..[["
+        repeat until value of text area 1 of group 5 of group 4 of group 1 of UI element 1 of scroll area 1 of group 1 of group 1 of window "DeepL" is not equal to previous_result and value of text area 1 of group 5 of group 4 of group 1 of UI element 1 of scroll area 1 of group 1 of group 1 of window "DeepL" is not equal to ""
+          #get the translated text
+        end repeat
+        return value of text area 1 of group 5 of group 4 of group 1 of UI element 1 of scroll area 1 of group 1 of group 1 of window "DeepL"
+      end tell
+    end tell
+    ]]
+  local app=hs.application.get('DeepL')
+  if not app then
+    local deepl = hs.application.launchOrFocus('DeepL')
+    --wait for the DeepL to launch
+    hs.timer.doAfter(0.5, function()
+    local success,result=hs.osascript.applescript(translate_script)
+    if success then
+      hs.pasteboard.setContents(result)
+    end
+    end)
+  else
+    print(translate_script)
+    local success,result=hs.osascript.applescript(translate_script)
+    hs.alert.show(result)
+    if success then
+      hs.alert.show("translate success")
+      hs.pasteboard.setContents(result)
+    end
+  end  
+
   hyper.triggered = true
 end)
 
@@ -66,6 +101,7 @@ hyper:bind({},"q", function()
   end
   hyper.triggered = true
 end)
+
 --test module
 -- hyper:bind({}, "c", function()
 --   -- menubar = hs.menubar.new(false)
@@ -90,10 +126,8 @@ hyper:bind({}, "c", function()
   local app=hs.application.get('Typora')
   local color=""
   local text=""
-
-  
   if app:isFrontmost() then
-    menubar = hs.menubar.new(true)
+    menubar = hs.menubar.new(false)
     menubar:setTitle("Hidden Menu")
     menubar:setMenu( {
         { title = "red", fn = function() color="red" end},
@@ -132,11 +166,40 @@ hyper2:bind({}, "q", function()
   hyper2.triggered = true
 end)
 
-hyper2:bind({}, "w", function()
-  --activate the window focused before
-  -- win = hs.window.find(win_id)
-  win:focus()
-  -- hs.alert.show("Window ID: "..win_id.." is activated")
+
+-- hyper2:bind({}, "a", function()
+--   --activate the deepL
+--   local app=hs.application.get('Deepl')
+--   hs.application.open("/Applications/Deepl.app")
+--   --enter the text in deepl input box
+--   local text="aaaa"
+--   local applescript=[[
+--     tell application "System Events"
+--       tell process "DeepL"
+--       set value of text area 1 of group 4 of group 2 of group 1 of UI element1 of scroll area 1 of group 1 of group 1 of window 1 of application process "DeepL" to "]]..text..[["
+--       end tell
+
+--   ]]
+--   local response=hs.osascript.applescript(applescript)
+--   hs.alert.show(response,0.5)
+
+--   hyper2.triggered = true
+-- end)
+
+hyper2:bind({}, "s", function()
+  local applescript=[[
+display dialog "表单" default answer "输入框内容" buttons {"按钮1", "按钮2", "按钮3"} default button 1 with icon caution
+copy the result as list to {text_returned, button_pressed} --返回一个列表{文本,按钮}
+
+    ]]
+    --get the list of the result applescript returns
+    local response
+    local list
+    response,list = hs.osascript.applescript(applescript)
+    print(response)
+    print(type(list))     
+    print(list[1])
+    print(list[2])
   hyper2.triggered = true
 end)
 
@@ -156,10 +219,20 @@ end)
 -- end)
 
 hyper:bind({}, "s", function()
-  hs.eventtap.keyStroke({'ctrl','cmd'}, '0')
+  --activate text scanner and scan the text
+  --if the text scanner is not running, start it
+  local app=hs.application.get('Text Scanner')
+  if not app then
+    local scanner = hs.application.launchOrFocus('Text Scanner')
+    --wait for the text scanner to launch
+    hs.timer.doAfter(0.5, function()
+      hs.eventtap.keyStroke({'ctrl','cmd'}, '0')
+    end)
+  else
+    hs.eventtap.keyStroke({'ctrl','cmd'}, '0')
+  end  
   hyper.triggered = true
 end)
-
 
 
 -- hyper:bind({}, "x", function()
